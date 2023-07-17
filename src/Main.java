0eb4f7f644cf6c7b4d40a4932d85bd3a78ad3270
thy <2632328358@qq.com>
2023-07-17 08:44:19 +0800

    初始提交

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        function func = new function();
+        func.turnOn();
+        func.turnOff();
+        System.out.println("这是在github的修改");
+    }
+}
---
+++ src/function.java
@@ -0,0 +1,8 @@
+public class function {
+    public void turnOn() {
+        System.out.println("正在开机");
+    }
+    public void turnOff() {
+        System.out.println("正在关机");
+    }
+}
